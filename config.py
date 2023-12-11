c46ce40f1e718b51d276408c3afb3b89f60a8260
Jyneda <jyneda@gmail.com>
2023-12-12 00:00:13 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+ServerURL='http://DomainName:9999'   
+dm_name = 'Sensor680'
+d_name = None
+
+MQTT_broker =  None
+MQTT_port = 1883
+MQTT_User = '?'
+MQTT_PW = '?'
+
+Comm_interval = 1 # unit:second
+
+odf_list = []
+
+idf_list = [
+    ('Temperature', float),
+    ('AtPressure', float),
+    ('Humidity', float),
+    ('Gas', float),
+    ]
+
+
